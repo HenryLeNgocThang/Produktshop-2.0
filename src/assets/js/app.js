4776e63ce5e5faa4720e0b00cbb994e85b63167a
Henry Le <35796147+HenryLeNgocThang@users.noreply.github.com>
2020-02-22 00:02:21 +0100

    rev

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,4 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#year").innerHTML = new Date().getFullYear();
 
     slider("#offers .le-slider-container", false);
+
+    var themeToggle = document.querySelector("#hero .label");
+
+    themeToggle.addEventListener("onclick", () => {
+        var modes = ["dark", "light"];
+        var state = modes[1];
+
+        if (state == modes[1]) {
+            state == modes[0];
+        } else {
+            state == modes[0];
+        }
+
+        console.log(state);
+    });
 });
