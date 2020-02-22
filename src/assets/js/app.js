51161de88fa68f9f922c27760458aed068c5c511
HenryLeNgocThang <35796147+HenryLeNgocThang@users.noreply.github.com>
2020-02-22 21:49:31 +0100

    rev

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelector("#year").innerHTML = new Date().getFullYear();
 
-    slider("#offers .le-slider-container", true, 10);
+    slider("#offers .le-slider-container");
 
     (function () {
         var themeToggle = document.querySelector("#hero .label input");
